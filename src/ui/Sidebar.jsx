eef5c36cc0a3b66055264f75d4ac43c525a06a8f
Heechem <hichempeter@gmail.com>
2023-08-01 09:29:00 -0600

    Add NavLink to the sidebar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,4 +1,6 @@
 import { styled } from 'styled-components';
+import Logo from './Logo';
+import MainNav from './MainNav';
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -8,7 +10,12 @@ const StyledSidebar = styled.aside`
 `;
 
 const Sidebar = () => {
-  return <StyledSidebar>SideBAr</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 };
 
 export default Sidebar;
