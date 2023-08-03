cb910c658783b91f4a97b7e8a0e05028488e18a3
Heechem <hichempeter@gmail.com>
2023-08-02 19:41:40 -0600

    Connecting supabase backend

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ewfbnthuhteqagzyuxke.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3ZmJudGh1aHRlcWFnenl1eGtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA5MzU1NTQsImV4cCI6MjAwNjUxMTU1NH0.63j7ioPtck4ZrMcO-spuzLln01ZTKF_cD7qz6aplr6c';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
