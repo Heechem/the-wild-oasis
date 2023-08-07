aa433710ff8ad02fa0b30300e3c2939c8c30616d
Heechem <hichempeter@gmail.com>
2023-08-07 11:25:55 -0600

    Add Form as a Modal window

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -2,6 +2,7 @@ import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import Spinner from '../../ui/Spinner';
+import { useEditSetting } from './useEditSetting';
 import { useSettings } from './useSetting';
 
 function UpdateSettingsForm() {
@@ -15,7 +16,15 @@ function UpdateSettingsForm() {
     } = {},
   } = useSettings();
 
+  const { isEditing, editSetting } = useEditSetting();
+
   if (isLoading) return <Spinner />;
+
+  function handleEdit(e, fieldName) {
+    const { value } = e.target;
+    if (!value) return;
+    editSetting({ [fieldName]: value });
+  }
 
   return (
     <Form>
@@ -24,27 +33,38 @@ function UpdateSettingsForm() {
           type="number"
           id="min-nights"
           defaultValue={minBookingLength}
+          disabled={isEditing}
+          onBlur={(e) => handleEdit(e, 'minBookingLength')}
         />
       </FormRow>
+
       <FormRow label="Maximum nights/booking">
         <Input
           type="number"
           id="max-nights"
           defaultValue={maxBookingLength}
+          onBlur={(e) => handleEdit(e, 'maxBookingLength')}
+          disabled={isEditing}
         />
       </FormRow>
+
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
+          disabled={isEditing}
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleEdit(e, 'maxGuestsPerBooking')}
         />
       </FormRow>
+
       <FormRow label="Breakfast price">
         <Input
           type="number"
           id="breakfast-price"
+          disabled={isEditing}
           defaultValue={BreakfastPrice}
+          onBlur={(e) => handleEdit(e, 'BreakfastPrice')}
         />
       </FormRow>
     </Form>
