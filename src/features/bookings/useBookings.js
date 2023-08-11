5292796073d1be91b80af637b41aae7330c44528
Heechem <hichempeter@gmail.com>
2023-08-11 09:33:34 -0600

    Add sorting bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -14,13 +14,21 @@ export function useBookings() {
       : { field: 'status', value: filteredValue };
   //{ field: 'totalPrice', value: 5000, method: 'gte' };
 
+  // Sort  bookings
+
+  const sortByRow = searchParams.get('sortBy') || 'startDate-desc';
+
+  const [field, direction] = sortByRow.split('-');
+
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, bookings, error };
 }
