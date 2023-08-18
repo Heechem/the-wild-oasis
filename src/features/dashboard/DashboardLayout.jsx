9f016a330e39e929890d7113dc803a74f9b7d2c7
Heechem <hichempeter@gmail.com>
2023-08-18 09:06:55 -0600

    Add dashbvoard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { useRecentBookings } from './useRecentBookings';
+import Spinner from '../../ui/Spinner';
+import { useRecentStays } from './useRecetStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,21 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+const DashboardLayout = () => {
+  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoadingStays } = useRecentStays();
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  console.log(confirmedStays);
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today activity</div>
+      <div>Chart</div>
+    </StyledDashboardLayout>
+  );
+};
+
+export default DashboardLayout;
